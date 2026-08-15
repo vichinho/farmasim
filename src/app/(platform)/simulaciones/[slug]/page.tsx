97a57ed427c219433942a3e2bb0c5e02c49d3e13
Vicente Andrés <vishoxcl@gmail.com>
2026-08-15 15:09:16 -0400

    style: route case 001 through polished action dock icons

--- src/app/(platform)/simulaciones/[slug]/page.tsx
+++ src/app/(platform)/simulaciones/[slug]/page.tsx
@@ -10,7 +10,7 @@ import {
   getTrainingModeByLevelId,
   trainingCases,
 } from "@/data/training";
-import { Case001ExperienceV4 } from "@/features/training/case001-experience-v4";
+import { Case001ExperienceV5 } from "@/features/training/case001-experience-v5";
 import { TrainingSession } from "@/features/training/training-session";
 import { loadTrainingLevels } from "@/features/training/load-training-levels";
 
@@ -84,8 +84,8 @@ export default async function TrainingCasePage({
         </header>
 
         {isInteractiveCase001 ? (
-          <Case001ExperienceV4
-            key={`${trainingMode.id}-v4`}
+          <Case001ExperienceV5
+            key={`${trainingMode.id}-v5`}
             levelNumber={trainingLevel.number}
             mode={trainingMode}
             trainingCase={trainingCase}
